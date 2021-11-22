7133d97029c1ab9f1f2fb1c55c2adf7d5bf0dd4f
Jamal S <jsaied99@gmail.com>
2021-11-22 14:35:29 -0500

    adding file input

--- main.java
+++ main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import java.nio.file.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+        System.out.println("Enter the name of the python file to parse: ");
+        Scanner scan = new Scanner(System.in);
+        String fileName = scan.nextLine();
+        Path path = Paths.get(fileName);
+
+        scan.close();
+        // check first if file ends with .py
+        if(!fileName.endsWith(".py")){
+            System.out.println("File name must end with .py");
+            return;
+        }
+        if (Files.notExists(path)) {
+            System.out.println("File does not exist");
+            return;
+        }
+        File fileObj = new File(fileName);
+        magicStuff(fileObj);
+
+
+        
+    
+
+    }
+
+    public static void magicStuff(File fileObj) throws IOException{
+        // yey
 
     }
 }
